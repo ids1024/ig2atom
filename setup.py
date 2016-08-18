d87652a44ea72b7bd44c0fe7cb7aedca2f9aeb0f
Ian D. Scott <ian@iandouglasscott.com>
2016-08-17 18:36:56 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ig2atom',
+      version='0.1',
+      description='Convert Instagram feed to atom',
+      author='Ian Douglas Scott',
+      author_email='ian@iandouglasscott.com',
+      url='https://github.com/ids1024/ig2atom',
+      scripts=['ig2atom']
+      )
